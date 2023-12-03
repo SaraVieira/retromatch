8211dbb17ac62f3cafd54209116eb4419c8fc695
Sara Vieira <hey@iamsaravieira.com>
2023-12-03 14:49:14 +0000

    add allowed in letsplay

--- main/helpers/index.ts
+++ main/helpers/index.ts
@@ -3,6 +3,31 @@ export * from "./create-window";
 import { customAlphabet } from "nanoid";
 import { alphanumeric } from "nanoid-dictionary";
 
+const allowedInLetsPlay = {
+  nes: 3,
+  snes: 4,
+  gb: 9,
+  gbc: 10,
+  gba: 12,
+  n64: 14,
+  md: 1,
+  gg: 21,
+  ms: 2,
+  pce: 31,
+  ps1: 57,
+  sega32: 19,
+  vb: 11,
+  sat: 22,
+  neo: 142,
+  gcn: 13,
+  atari2600: 26,
+  panasonic3DO: 29,
+  cdi: 133,
+  jaguar: 27,
+  ngp: 25,
+  lynx: 28,
+};
+
 export const transformResponse = (data: any, type: string) => {
   if (!data) return null;
   if (type === "arcadeDB") {
