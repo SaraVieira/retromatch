d3a47cbe784277f0e244c0c0a1831a94d5addc7a
Sara Vieira <hey@iamsaravieira.com>
2023-12-04 21:52:30 +0000

    add link to new

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -3,6 +3,8 @@ import { useFolders } from "../hooks/folder-context";
 
 import { Card, CardBody, CardHeader, Spinner } from "@nextui-org/react";
 import { useRouter } from "next/router";
+import { IconPlus } from "@tabler/icons-react";
+import Link from "next/link";
 
 export default function HomePage() {
   const { folders, isLoading } = useFolders();
@@ -22,7 +24,7 @@ export default function HomePage() {
       <ul className="flex items-stretch flex-wrap gap-4 py-8">
         {Object.values(folders).map((f) => (
           <li className="w-[200px]" key={f.id}>
-            <button onClick={() => onClick(f)}>
+            <button className="w-full" onClick={() => onClick(f)}>
               <Card>
                 <CardHeader>{f.name}</CardHeader>
                 <CardBody className="text-xs text-content4">{f.path}</CardBody>
@@ -30,6 +32,16 @@ export default function HomePage() {
             </button>
           </li>
         ))}
+        <li className="w-[200px]">
+          <Link href={"/new"}>
+            <Card>
+              <CardBody className="flex flex-col gap-4 items-center justify-center">
+                <IconPlus />
+                Add new
+              </CardBody>
+            </Card>
+          </Link>
+        </li>
       </ul>
     </div>
   );
