b86bcde74eee6919336c3b81f1a604d17a800fa2
Sara Vieira <hey@iamsaravieira.com>
2023-12-09 17:24:27 +0100

    add subscriptions

--- main/preload.ts
+++ main/preload.ts
@@ -4,6 +4,18 @@ const handler = {
   send(channel: string, value: unknown) {
     ipcRenderer.send(channel, value);
   },
+  subscribe(channel: string, callback: (...args: unknown[]) => void) {
+    const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+      callback(...args);
+    ipcRenderer.on(channel, subscription);
+  },
+
+  addListener(channel: string, callback: (...args: unknown[]) => void) {
+    const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+      callback(...args);
+    ipcRenderer.addListener(channel, subscription);
+  },
+
   on(channel: string, callback: (...args: unknown[]) => void) {
     const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
       callback(...args);
@@ -16,5 +28,7 @@ const handler = {
 };
 
 contextBridge.exposeInMainWorld("ipc", handler);
+contextBridge.exposeInMainWorld("ipcRenderer", ipcRenderer);
 
 export type IpcHandler = typeof handler;
+s;
