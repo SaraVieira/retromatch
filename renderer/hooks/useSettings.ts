c22d857995be9718f7c1709425471b204d170153
Sara Vieira <hey@iamsaravieira.com>
2023-12-13 13:19:48 +0000

    add import & export

---
+++ renderer/hooks/useSettings.ts
@@ -0,0 +1,37 @@
+import { useRouter } from "next/router";
+import toast from "react-hot-toast";
+
+export const useSettings = () => {
+  const router = useRouter();
+  const onClearCache = () => {
+    window.ipc.send("clear-cache", null);
+
+    window.ipc.once("done-cache-clear", () => {
+      toast.success("Cache cleared");
+      window.ipc.send("load", null);
+      router.push("/");
+    });
+  };
+
+  const onExportData = () => {
+    window.ipc.send("export-data", null);
+    window.ipc.once("exported", () => {
+      toast.success("Data exported");
+    });
+  };
+
+  const onImportData = () => {
+    window.ipc.send("import-data", null);
+    window.ipc.once("imported", () => {
+      toast.success("Data imported");
+      window.ipc.send("load", null);
+      router.push("/");
+    });
+  };
+
+  return {
+    onClearCache,
+    onExportData,
+    onImportData
+  };
+};
