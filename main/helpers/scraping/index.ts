d7a964948bafa2e400a65593c6f9acedf42174c2
Ramón Huidobro <hola@ramonh.dev>
2023-12-23 18:06:49 +0100

    Safe game info on cloudflare

--- main/helpers/scraping/index.ts
+++ main/helpers/scraping/index.ts
@@ -1,33 +1,66 @@
 import axios from "axios";
-import { transformResponse } from "./response-transform";
-import { allowedInLetsPlay } from "../constants";
 import axiosRetry from "axios-retry";
+import { allowedInLetsPlay } from "../constants";
+import { transformResponse } from "./response-transform";
 
 axiosRetry(axios, { retryDelay: axiosRetry.exponentialDelay, retries: 3 });
 
+const workerUrl = "http://localhost:8787/scrape";
+
 export const scrapeGame = async (file: any, scraping_id: number) => {
   const normalizedName = file.name.replaceAll(/\s*\(.*?\)/gi, "").trim();
-  if (scraping_id === 75) {
-    const response = await axios(
-      `http://adb.arcadeitalia.net/service_scraper.php?ajax=query_mame&lang=en&use_parent=1&game_name=${file.name}`
-    ).then((rsp) => rsp.data);
-    if (response.result[0]) {
-      return transformResponse(response.result[0], "arcadeDB");
-    }
-  }
+  try {
+    const gameInfo = await axios(`${workerUrl}?id=${file.id}`).then(
+      (rsp) => rsp.data
+    );
+    if (!gameInfo) {
+      if (scraping_id === 75) {
+        const response = await axios(
+          `http://adb.arcadeitalia.net/service_scraper.php?ajax=query_mame&lang=en&use_parent=1&game_name=${file.name}`
+        ).then((rsp) => rsp.data);
+        if (response.result[0]) {
+          const transformedResponse = transformResponse(
+            response.result[0],
+            "arcadeDB"
+          );
+          await axios.post(
+            workerUrl,
+            JSON.stringify({ id: file.id, info: transformedResponse })
+          );
+          return transformedResponse;
+        }
+      }
 
-  if (allowedInLetsPlay.map((a) => a.id).includes(scraping_id)) {
-    const response = await axios(
-      `https://letsplayretro.games/api/scrape?query=${encodeURI(
-        normalizedName
-      )}&console=${allowedInLetsPlay.find((c) => c.id === scraping_id).name}`
-    ).then((rsp) => rsp.data);
-    if (response) {
-      // wait because prisma cries if we don't
-      await new Promise((resolve) => setTimeout(resolve, 5000));
+      if (allowedInLetsPlay.map((a) => a.id).includes(scraping_id)) {
+        const response = await axios(
+          `https://letsplayretro.games/api/scrape?query=${encodeURI(
+            normalizedName
+          )}&console=${
+            allowedInLetsPlay.find((c) => c.id === scraping_id).name
+          }`
+        ).then((rsp) => rsp.data);
+        if (response) {
+          // wait because prisma cries if we don't
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+          if (response) {
+            // wait because prisma cries if we don't
+            await new Promise((resolve) => setTimeout(resolve, 5000));
 
-      return transformResponse(response, "letsplay");
+            const transformedResponse = transformResponse(response, "letsplay");
+            await axios.post(
+              workerUrl,
+              JSON.stringify({ id: file.id, info: transformedResponse })
+            );
+            return transformedResponse;
+          }
+        }
+      } else {
+        console.log("Got info:", gameInfo);
+        return gameInfo;
+      }
     }
+  } catch (e) {
+    console.log(e.message);
   }
 
   const screenscraperInfo = await axios(
--- workers/scrape.ts
+++ workers/scrape.ts
@@ -7,21 +7,23 @@ type GameInfo = {
   info?: any;
 };
 
-const handler: ExportedHandler = {
+const handle: ExportedHandler = {
   async fetch(request: Request, env: Env, _ctx) {
-    const { id, info } = (await request.json()) as GameInfo;
     if (request.method === "POST") {
-      await env.retromatch.put(id, info);
-      return new Response(info);
+      const { id, info } = (await request.json()) as GameInfo;
+      const newInfo = JSON.stringify(info);
+      await env.retromatch.put(id, newInfo);
+      return new Response(newInfo);
     } else {
+      const id = new URLSearchParams(new URL(request.url).search).get("id");
       const value = await env.retromatch.get(id);
       if (value) {
         return new Response(value);
       } else {
-        return new Response("Value not found", { status: 404 });
+        return new Response();
       }
     }
   }
 };
 
-export default handler;
+export default handle;
