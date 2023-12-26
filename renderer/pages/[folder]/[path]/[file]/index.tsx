07f8f5ca27cb90322ae86f1deb07a82e2910e051
Ramón Huidobro <hola@ramonh.dev>
2023-12-26 18:09:15 +0100

    Add header

--- renderer/pages/[folder]/[path]/[file]/index.tsx
+++ renderer/pages/[folder]/[path]/[file]/index.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+
 import { useRoms } from "../../../../hooks/roms-context";
 
 export const Files = () => {
@@ -8,10 +9,24 @@ export const Files = () => {
   const activeFile = roms[query.file as string];
 
   return (
-    <div className="container mx-auto">
-      <video src={activeFile?.info?.videos?.shortplay} controls />
-      <pre>{JSON.stringify(activeFile, null, 2)}</pre>
-    </div>
+    <>
+      <div
+        className="backdrop-saturate-150 bg-background/90 backdrop-blur-sm -mt-6 -ml-6 p-6 border-b border-divider w-screen sticky top-0 z-[99] overflow-hidden"
+        style={{
+          height: 90,
+          width: "calc(100% + 3rem)"
+        }}
+      >
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-bold"> {activeFile?.info?.title}</h1>
+          <div className="flex items-center gap-4"></div>
+        </div>
+      </div>
+      <div className="container mx-auto mt-4">
+        <video src={activeFile?.info?.videos?.shortplay} controls />
+        <pre>{JSON.stringify(activeFile, null, 2)}</pre>
+      </div>
+    </>
   );
 };
 
